26ffc9b0f986811b0bf1227c1e3711333e32d5a1
Syed Thahir <34040173+Syedmohammadthahir@users.noreply.github.com>
2018-12-18 12:22:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// const box = document.querySelector('.box');
+// box.style.display = 'none';
+
+
+// jQuery('.box').hide();
+// jQuery('.box').show();
+
+// $('.box').show();
+// $('.box').hide();
+
+// ('.box').hide();
+
+
+// box.addEventListener('click', function(){
+// 	alert('You clicked the rectangle');
+// });
+
+$('.box').click(function(){
+	alert('You clicked the rectangle')
+});
+
+
+
+
+
+
+
+
